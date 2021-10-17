a261b5b2fd8198eb42dab645c4537e11ffeab2fa
Rodrigo Escandón Cesarman <rodrigo.escandon.cesarman@gmail.com>
2021-10-17 16:45:43 -0500

    Initial commit. Using yhaefliger/YATAS.

---
+++ .eleventy.js
@@ -0,0 +1,37 @@
+const htmlmin = require('html-minifier')
+
+
+module.exports = function(eleventyConfig) {
+  /**
+   * Files to copy
+   * https://www.11ty.dev/docs/copy/
+   */
+  eleventyConfig.addPassthroughCopy('src/img')
+
+  /**
+   * HTML Minifier for production builds
+   */
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (
+      process.env.ELEVENTY_ENV == 'production' &&
+      outputPath &&
+      outputPath.endsWith('.html')
+    ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      })
+      return minified
+    }
+
+    return content
+  })
+
+  return {
+    dir: {
+      input: "src",
+      data: "../_data"
+    }
+  };
+};
